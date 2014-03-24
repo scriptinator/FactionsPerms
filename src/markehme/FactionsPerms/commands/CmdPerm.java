c1435deb5b2d8596487667572e0e39392f013882
MarkehMe <mark@markeh.me>
2014-03-24 14:03:31 +1100

    Adding default command structure

--- src/markehme/FactionsPerms/commands/CmdPerm.java
+++ src/markehme/FactionsPerms/commands/CmdPerm.java
@@ -1,5 +1,9 @@
 package markehme.FactionsPerms.commands;
 
+import markehme.FactionsPerms.FactionsPermsAPI;
+
+import org.bukkit.ChatColor;
+
 import com.massivecraft.factions.cmd.FCommand;
 import com.massivecraft.factions.cmd.req.ReqFactionsEnabled;
 
@@ -17,8 +21,49 @@ public class CmdPerm extends FCommand {
 
 	@Override
 	public void perform() {
-			
+		FactionsPermsAPI api = new FactionsPermsAPI();
 		
+
+		
+		if(args.size() == 1) {
+			if(args.get(0).equals("reload")) {
+				
+				if(!api.hasPermission(usender.getName(), "factionsperms.reload") && !usender.isConsole()) {
+					msg(ChatColor.RED + "You do not have the required permission to run this command.");
+					return;
+				}
+				
+				msg("reload commencing ... ");
+				return;
+			}
+			
+			if(args.get(0).equals("ihave")) {
+				if(!api.hasPermission(usender.getName(), "factionsperms.ihave") && !usender.isConsole()) {
+					msg(ChatColor.RED + "You do not have the required permission to run this command.");
+					return;
+				}
+				msg(ChatColor.DARK_RED + "Please specificy the permission!");
+				return;
+			}
+			
+		}
+		
+		if(args.size() > 1) {
+			if(args.get(0).equals("ihave")) {
+				if(!api.hasPermission(usender.getName(), "factionsperms.ihave") && !usender.isConsole()) {
+					msg(ChatColor.RED + "You do not have the required permission to run this command.");
+					return;
+				}
+				
+				if(api.hasPermission(usender.getName(), args.get(1))) {
+					msg(ChatColor.GREEN + args.get(1) + " - yes");
+				} else {
+					msg(ChatColor.RED   + args.get(1) + " - no");
+				}
+				
+				return;
+			}
+		}
 			
 	}
 }
