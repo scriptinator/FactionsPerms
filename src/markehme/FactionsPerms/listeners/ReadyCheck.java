834c1c4968ac9aeb642b1dbf125c6aebd59bf963
MarkehMe <mark@markeh.me>
2014-03-24 20:07:26 +1100

    Add no-defined users to the 'default' group

--- src/markehme/FactionsPerms/listeners/ReadyCheck.java
+++ src/markehme/FactionsPerms/listeners/ReadyCheck.java
@@ -1,5 +1,9 @@
 package markehme.FactionsPerms.listeners;
 
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.logging.Level;
+
 import markehme.FactionsPerms.FactionsPerms;
 
 import org.bukkit.event.EventHandler;
@@ -22,7 +26,17 @@ public class ReadyCheck implements Listener {
 		}
 		
 		if(!FactionsPerms.userSet.containsKey(event.getPlayer().getName().toLowerCase())) {
+			FactionsPerms.get().getLogger().log(Level.INFO, "Adding " + event.getPlayer().getName() + " to default group");
+			FactionsPerms.usersConfig.set("Users." + event.getPlayer().getName() + ".groups", Arrays.asList("default"));
+			FactionsPerms.usersConfig.set("Users." + event.getPlayer().getName() + ".permissions", Arrays.asList(""));
 			
+			FactionsPerms.loadUser(event.getPlayer().getName());
+			
+			try {
+				FactionsPerms.usersConfig.save(FactionsPerms.usersConfigFile);
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
 		}
 	}
 }
