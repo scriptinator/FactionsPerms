780b6f45dfcb0d475070ec810c9fa4908c145fc5
MarkehMe <mark@markeh.me>
2014-03-24 14:01:53 +1100

    PermissionUser now using HashMap

--- src/markehme/FactionsPerms/obj/PermissionUser.java
+++ src/markehme/FactionsPerms/obj/PermissionUser.java
@@ -1,6 +1,6 @@
 package markehme.FactionsPerms.obj;
 
-import java.util.Map;
+import java.util.HashMap;
 import java.util.Map.Entry;
 
 import markehme.FactionsPerms.FactionsPerms;
@@ -12,10 +12,10 @@ import markehme.FactionsPerms.FactionsPerms;
  *
  */
 public class PermissionUser {
-	public Map<String, Boolean> validPerms;
-	public Map<String, Group> inGroups;
+	public HashMap<String, Boolean> validPerms;
+	public HashMap<String, Group> inGroups;
 	
-	public PermissionUser(String PlayerName, Map<String, String> playerpermissions, Map<String, String> groups) {
+	public PermissionUser(String PlayerName, HashMap<String, String> playerpermissions, HashMap<String, String> groups) {
 		
 		// Store all groups 
 		for(Entry<String, Group> entry : FactionsPerms.permissionsSet.entrySet()) {
@@ -28,13 +28,16 @@ public class PermissionUser {
 			//String key = entry.getKey();
 			String value = entry.getValue().toLowerCase();
 			
-			// Remove any existing value 
-			validPerms.remove(value);
-			
 			if(value.startsWith("-")) {
+				// Remove existing permission
+				validPerms.remove(value.substring(1));
+				
 				// is a negative permission, so make it a false value 
 				validPerms.put(value.substring(1), false);
 			} else {
+				// Remove existing permission
+				validPerms.remove(value);
+				
 				// is a normal permission, so make a true value 
 				validPerms.put(value, true);
 			}
