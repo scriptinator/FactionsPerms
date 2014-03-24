d4b55312b249911397b5fcfa12742566ad6b1dcc
MarkehMe <mark@markeh.me>
2014-03-24 23:46:13 +1100

    Move to HashMap's, adding missing values

--- src/markehme/FactionsPerms/obj/Group.java
+++ src/markehme/FactionsPerms/obj/Group.java
@@ -1,34 +1,35 @@
 package markehme.FactionsPerms.obj;
 
-import java.util.Map;
+import java.util.HashMap;
 
 public class Group {
-	public Map<String, Boolean> Permissions_Global			= null;
-	public Map<String, Boolean> Permissions_Current		= null;
-	public Map<String, Boolean> Permissions_Ally 			= null;
-	public Map<String, Boolean> Permissions_Neutral 		= null;
-	public Map<String, Boolean> Permissions_Enemy 			= null;
-	public Map<String, Boolean> Permissions_Safezone 		= null;
-	public Map<String, Boolean> Permissions_Warzone 		= null;
-	public Map<String, Boolean> Permissions_Wilderness		= null;
+	public HashMap<String, Boolean> Permissions_Global			= new HashMap<String, Boolean>();
+	public HashMap<String, Boolean> Permissions_Current			= new HashMap<String, Boolean>();
+	public HashMap<String, Boolean> Permissions_Ally 			= new HashMap<String, Boolean>();
+	public HashMap<String, Boolean> Permissions_Neutral 		= new HashMap<String, Boolean>();
+	public HashMap<String, Boolean> Permissions_Enemy 			= new HashMap<String, Boolean>();
+	public HashMap<String, Boolean> Permissions_Safezone 		= new HashMap<String, Boolean>();
+	public HashMap<String, Boolean> Permissions_Warzone 		= new HashMap<String, Boolean>();
+	public HashMap<String, Boolean> Permissions_Wilderness		= new HashMap<String, Boolean>();
 		
 	public Group(String name,
-			Map<String, Boolean> Perms_Global,
-			Map<String, Boolean> Perms_Current,
-			Map<String, Boolean> Perms_Ally,
-			Map<String, Boolean> Perms_Neutral,
-			Map<String, Boolean> Perms_Enemy,
-			Map<String, Boolean> Perms_Safezone,
-			Map<String, Boolean> Perms_Warzone, 
-			Map<String, Boolean> Perms_Wilderness) {
+			HashMap<String, Boolean> Perms_Global,
+			HashMap<String, Boolean> Perms_Current,
+			HashMap<String, Boolean> Perms_Ally,
+			HashMap<String, Boolean> Perms_Neutral,
+			HashMap<String, Boolean> Perms_Enemy,
+			HashMap<String, Boolean> Perms_Safezone,
+			HashMap<String, Boolean> Perms_Warzone, 
+			HashMap<String, Boolean> Perms_Wilderness) {
 		
 		Permissions_Global				= Perms_Global;
 		Permissions_Current				= Perms_Current;
 		Permissions_Ally				= Perms_Ally;
 		Permissions_Neutral				= Perms_Neutral;
-		Permissions_Safezone			= Perms_Neutral;
-		Permissions_Warzone				= Perms_Neutral;
-		Permissions_Wilderness			= Perms_Neutral;
+		Permissions_Enemy				= Perms_Enemy;
+		Permissions_Safezone			= Perms_Safezone;
+		Permissions_Warzone				= Perms_Warzone;
+		Permissions_Wilderness			= Perms_Wilderness;
 		
 	}
 }
