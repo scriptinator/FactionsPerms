c471eeb25fd5166341c6356c3043ebbaa2b6882d
MarkehMe <mark@markeh.me>
2014-03-24 13:57:40 +1100

    Documented code, also now loading groups

--- src/markehme/FactionsPerms/FactionsPerms.java
+++ src/markehme/FactionsPerms/FactionsPerms.java
@@ -4,7 +4,9 @@ import java.io.File;
 import java.io.IOException;
 import java.util.Arrays;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.Set;
 import java.util.logging.Level;
 
 import markehme.FactionsPerms.listeners.ReadyCheck;
@@ -60,19 +62,30 @@ public class FactionsPerms extends JavaPlugin {
 		permissionsConfigFile = new File(getDataFolder(), "permissions.yml");
 		usersConfigFile = new File(getDataFolder(), "users.yml");
 		
+		// Save default config.yml
 		saveDefaultConfig();
 		
+		// Load permissions file
 		permissionsConfig = YamlConfiguration.loadConfiguration(permissionsConfigFile);
 		
+		// Make sure it exists
 		if(!permissionsConfigFile.exists()) {
 			createDefaultPermissionsFile();
 		}
 		
+		// Load the permissions file 
+		loadPermissions();
+		
+		// Load users file
 		usersConfig = YamlConfiguration.loadConfiguration(usersConfigFile);
 		
+		// Make sure it exists
 		if(!usersConfigFile.exists()) {
 			createDefaultUsersFile();
 		}
+		
+		// Load the users file
+		loadUsers();
 		
 		if(getConfig().getBoolean("disallowConnectionsUntilReady")) {
 			getServer().getPluginManager().registerEvents(new ReadyCheck(), this);
@@ -119,6 +132,205 @@ public class FactionsPerms extends JavaPlugin {
 		} catch (IOException e) {
 			this.getLogger().log(Level.INFO, "Failed to create a default users.yml file. Do we have permission?");
 			e.printStackTrace();
+		}
+	}
+	
+	/**
+	 * Load the permissions.yml file 
+	 */
+	private void loadPermissions() {
+		Set<String> groups = permissionsConfig.getConfigurationSection("Permissions").getKeys(false);
+				
+		int groupCount = 0;
+		for(String group : groups) {
+			groupCount++;
+						
+			// All the HashMaps for a group
+			HashMap<String, Boolean> Permissions_Global		= new HashMap<String, Boolean>();
+			HashMap<String, Boolean> Permissions_Current	= new HashMap<String, Boolean>();
+			HashMap<String, Boolean> Permissions_Ally 		= new HashMap<String, Boolean>();
+			HashMap<String, Boolean> Permissions_Neutral 	= new HashMap<String, Boolean>();
+			HashMap<String, Boolean> Permissions_Enemy 		= new HashMap<String, Boolean>();
+			HashMap<String, Boolean> Permissions_Safezone 	= new HashMap<String, Boolean>();
+			HashMap<String, Boolean> Permissions_Warzone 	= new HashMap<String, Boolean>();
+			HashMap<String, Boolean> Permissions_Wilderness	= new HashMap<String, Boolean>();
+			
+			// Global permissions 
+			if(!permissionsConfig.getList("Permissions."+group+".global").isEmpty()) {
+				List<?> globalPerms = permissionsConfig.getList("Permissions."+group+".global");
+				
+				for (int i=0;i<globalPerms.size();i++) {
+					String perm = (String) globalPerms.get(i);
+					
+					if(perm.startsWith("-")) {
+						if(Permissions_Global.containsKey(perm.substring(1))) {
+							Permissions_Global.remove(perm.substring(1));
+						}
+						
+						Permissions_Global.put(perm.substring(1), false);
+					} else {
+						Permissions_Global.put(perm, true);
+					}
+				}
+			}
+			
+			// Factions.current
+			if(!permissionsConfig.getList("Permissions."+group+".factions.current").isEmpty()) {
+				List<?> fCurrentPerms = permissionsConfig.getList("Permissions."+group+".factions.current");
+				
+				for (int i = 0; i < fCurrentPerms.size(); i++) {
+					String perm = (String) fCurrentPerms.get(i);
+					
+					if(perm.startsWith("-")) {
+						if(Permissions_Current.containsKey(perm.substring(1))) {
+							Permissions_Current.remove(perm.substring(1));
+						}
+						
+						Permissions_Current.put(perm.substring(1), false);
+					} else {
+						Permissions_Current.put(perm, true);
+					}
+				}
+			}
+			
+			// Factions.ally
+			if(!permissionsConfig.getList("Permissions."+group+".factions.ally").isEmpty()) {
+				List<?> fCurrentPerms = permissionsConfig.getList("Permissions."+group+".factions.ally");
+				
+				for (int i = 0; i < fCurrentPerms.size(); i++) {
+					String perm = (String) fCurrentPerms.get(i);
+					
+					if(perm.startsWith("-")) {
+						if(Permissions_Ally.containsKey(perm.substring(1))) {
+							Permissions_Ally.remove(perm.substring(1));
+						}
+						
+						Permissions_Ally.put(perm.substring(1), false);
+					} else {
+						Permissions_Ally.put(perm, true);
+					}
+				}
+			}
+			
+			// factions.neutral
+			if(!permissionsConfig.getList("Permissions."+group+".factions.neutral").isEmpty()) {
+				List<?> fCurrentPerms = permissionsConfig.getList("Permissions."+group+".factions.neutral");
+				
+				for (int i = 0; i < fCurrentPerms.size(); i++) {
+					String perm = (String) fCurrentPerms.get(i);
+					
+					if(perm.startsWith("-")) {
+						if(Permissions_Neutral.containsKey(perm.substring(1))) {
+							Permissions_Neutral.remove(perm.substring(1));
+						}
+						
+						Permissions_Neutral.put(perm.substring(1), false);
+					} else {
+						Permissions_Neutral.put(perm, true);
+					}
+				}
+			}
+			
+			
+			// factions.enemy
+			if(!permissionsConfig.getList("Permissions."+group+".factions.enemy").isEmpty()) {
+				List<?> fCurrentPerms = permissionsConfig.getList("Permissions."+group+".factions.enemy");
+				
+				for (int i = 0; i < fCurrentPerms.size(); i++) {
+					String perm = (String) fCurrentPerms.get(i);
+					
+					if(perm.startsWith("-")) {
+						if(Permissions_Enemy.containsKey(perm.substring(1))) {
+							Permissions_Enemy.remove(perm.substring(1));
+						}
+						
+						Permissions_Enemy.put(perm.substring(1), false);
+					} else {
+						Permissions_Enemy.put(perm, true);
+					}
+				}
+			}
+			
+			// factions.safezone
+			if(!permissionsConfig.getList("Permissions."+group+".factions.safezone").isEmpty()) {
+				List<?> fCurrentPerms = permissionsConfig.getList("Permissions."+group+".factions.safezone");
+				
+				for (int i = 0; i < fCurrentPerms.size(); i++) {
+					String perm = (String) fCurrentPerms.get(i);
+					
+					if(perm.startsWith("-")) {
+						if(Permissions_Safezone.containsKey(perm.substring(1))) {
+							Permissions_Safezone.remove(perm.substring(1));
+						}
+						
+						Permissions_Safezone.put(perm.substring(1), false);
+					} else {
+						Permissions_Safezone.put(perm, true);
+					}
+				}
+			}
+
+			
+			// factions.warzone
+			if(!permissionsConfig.getList("Permissions."+group+".factions.warzone").isEmpty()) {
+				List<?> fCurrentPerms = permissionsConfig.getList("Permissions."+group+".factions.warzone");
+				
+				for (int i = 0; i < fCurrentPerms.size(); i++) {
+					String perm = (String) fCurrentPerms.get(i);
+					
+					if(perm.startsWith("-")) {
+						if(Permissions_Warzone.containsKey(perm.substring(1))) {
+							Permissions_Warzone.remove(perm.substring(1));
+						}
+						
+						Permissions_Warzone.put(perm.substring(1), false);
+					} else {
+						Permissions_Warzone.put(perm, true);
+					}
+				}
+			}
+			
+			// factions.wilderness
+			if(!permissionsConfig.getList("Permissions."+group+".factions.wilderness").isEmpty()) {
+				List<?> fCurrentPerms = permissionsConfig.getList("Permissions."+group+".factions.wilderness");
+				
+				for (int i = 0; i < fCurrentPerms.size(); i++) {
+					String perm = (String) fCurrentPerms.get(i);
+					
+					if(perm.startsWith("-")) {
+						if(Permissions_Wilderness.containsKey(perm.substring(1))) {
+							Permissions_Wilderness.remove(perm.substring(1));
+						}
+						
+						Permissions_Wilderness.put(perm.substring(1), false);
+					} else {
+						Permissions_Wilderness.put(perm, true);
+					}
+				}
+			}
+
+		}
+		
+		if(groupCount > 1) {
+			this.getLogger().log(Level.INFO, "Loaded " + groupCount + " group's");
+		} else if(groupCount == 0) {
+			this.getLogger().log(Level.INFO, "No groups loaded");
+		} else {
+			this.getLogger().log(Level.INFO, "Loaded 1 group");
+		}
+		
+		
+	}
+	
+	/**
+	 * Load the users.yml file
+	 */
+	private void loadUsers() {
+		Set<String> users = usersConfig.getConfigurationSection("Users").getKeys(false);
+		
+		int userCount = 0;
+		for(String user : users) {
+			
 		}
 	}
 }
